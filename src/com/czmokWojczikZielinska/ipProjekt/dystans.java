4936f6260232d25ac843532716af57e2006565dd
czmok <czmokuu@gmail.com>
2014-05-21 15:25:15 +0200

    Aniu jak mozesz to sprwadz czy dobrze dodalam te wartosci do bazy danych to jest w funkcji dodajDoBazyDanych. aplikacja sie konczy i wysyla powiadominie jak wcisniemy guzik koniec potem to zmienie ze bedzie wysylac powiadomienie jak przebiegnie sie odpowiedni dystans bo teraz nie chce mi sie wychodzic z domu i przechodzic dystansu; p a tak to chyba juz dziala w miare poprawnie bo duzy blad jest mimo wszystko

---
+++ src/com/czmokWojczikZielinska/ipProjekt/dystans.java
@@ -0,0 +1,11 @@
+package com.czmokWojczikZielinska.ipProjekt;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: magda
+ * Date: 08.05.14
+ * Time: 21:47
+ * To change this template use File | Settings | File Templates.
+ */
+public class dystans {
+}
